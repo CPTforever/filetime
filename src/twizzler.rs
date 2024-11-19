99598cb283aa954277597ea995a6c6feb4ceea84
Albert Lee <allilee@ucsc.edu>
2024-11-19 09:30:35 -0800

    added a no-ops

--- src/twizzler.rs
+++ src/twizzler.rs
@@ -4,31 +4,40 @@ use std::io;
 use std::path::Path;
 
 pub fn set_file_times(_p: &Path, _atime: FileTime, _mtime: FileTime) -> io::Result<()> {
-    Err(io::Error::new(io::ErrorKind::Other, "Wasm not implemented"))
+    Ok(())
 }
 
 pub fn set_symlink_file_times(_p: &Path, _atime: FileTime, _mtime: FileTime) -> io::Result<()> {
-    Err(io::Error::new(io::ErrorKind::Other, "Wasm not implemented"))
+    Ok(())
 }
 
 pub fn set_file_mtime(_p: &Path, _mtime: FileTime) -> io::Result<()> {
-    Err(io::Error::new(io::ErrorKind::Other, "Wasm not implemented"))
+    Ok(())
 }
 
 pub fn set_file_atime(_p: &Path, _atime: FileTime) -> io::Result<()> {
-    Err(io::Error::new(io::ErrorKind::Other, "Wasm not implemented"))
+    Ok(())
 }
 
 pub fn from_last_modification_time(_meta: &fs::Metadata) -> FileTime {
-    unimplemented!()
+    FileTime {
+        seconds: 0,
+        nanos: 0,
+    }
 }
 
 pub fn from_last_access_time(_meta: &fs::Metadata) -> FileTime {
-    unimplemented!()
+    FileTime {
+        seconds: 0,
+        nanos: 0,
+    }
 }
 
 pub fn from_creation_time(_meta: &fs::Metadata) -> Option<FileTime> {
-    unimplemented!()
+    Some(FileTime {
+        seconds: 0,
+        nanos: 0,
+    })
 }
 
 pub fn set_file_handle_times(
@@ -36,5 +45,5 @@ pub fn set_file_handle_times(
     _atime: Option<FileTime>,
     _mtime: Option<FileTime>,
 ) -> io::Result<()> {
-    Err(io::Error::new(io::ErrorKind::Other, "Wasm not implemented"))
+    Ok(())
 }
